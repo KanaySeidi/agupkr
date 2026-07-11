816b5b0f41c6a48d88c73743c5d36cef1722b058
Alida S. <asulaimanova@doscredobank.kg>
2026-07-11 17:56:19 +0600

    changed aplicant text and img

--- src/components/pages/aplicant/Aplicant.tsx
+++ src/components/pages/aplicant/Aplicant.tsx
@@ -7,17 +7,33 @@ import { useNavigate } from "react-router-dom";
 const Aplicant = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const currentYear = new Date().getFullYear();
+
   return (
     <>
-      <div>
-        <img src={aplicant} alt={t("auto.components.pages.aplicant.Aplicant.1")} className="rounded-md w-full object-cover" />
+      <div className="relative w-full">
+        <img
+            src={aplicant}
+            alt={`Абитуриент ${currentYear}`}
+            className="rounded-md w-full object-cover"
+        />
+
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="text-center text-white">
+            <h1 className="text-3xl sm:text-5xl lg:text-6xl font-medium">
+              Абитуриент {currentYear}
+            </h1>
+
+            <div className="w-40 sm:w-72 h-[2px] bg-white mt-4 sm:mt-6 mx-auto"/>
+          </div>
+        </div>
       </div>
       <div className="mt-8 sm:mt-10 flex flex-col gap-4 sm:gap-5">
         <h1 className="text-xl sm:text-2xl w-full">{t("admission.title")}</h1>
         <p className="w-full sm:w-2/3 text-sm sm:text-base">{t("admission.subtitle")}</p>
       </div>
       <div className="rounded-md bg-[#F7F9FF] px-3 sm:px-5 py-5 flex flex-col gap-5 mt-8 sm:mt-10">
-        <Line title={t("admission.blockTitle")} />
+        <Line title={t("admission.blockTitle")}/>
         <p className="text-sm sm:text-base">{t("admission.desc")}</p>
         <p className="text-sm sm:text-base">{t("admission.direction")}</p>
         <div>
